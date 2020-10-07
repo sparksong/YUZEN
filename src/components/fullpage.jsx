4bc3c5f2d5441c47236469b22f3343022f5e5a3a
gracemkas <gracekasahara@gmail.com>
2020-10-06 22:08:23 -0500

    added fullpage.js and slogan

---
+++ src/components/fullpage.jsx
@@ -0,0 +1,10 @@
+//export default FullPage = () => {
+    /*new fullpage('#fullpage', {
+        //options here
+        autoScrolling:true,
+        scrollHorizontally: true
+    })*/
+
+    //methods
+    //fullpage_api.setAllowScrolling(false)
+//}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,10 +2,12 @@
 import Head from '../components/head'
 import Nav from '../components/nav'
 import Footer from '../components/footer'
+import Slogan from '../components/slogan'
 import Welcome from '../components/welcome'
 import Translation from '../components/translation'
 import Proofreading from '../components/proofreading'
 import PresentationCoaching from '../components/presentation-coaching'
+//import FullPage from '../components/fullpage'
 
 const siteTitle = 'YUZEN Translation'
 const siteDescription =
@@ -24,6 +26,7 @@ const MainApp = () => (
 		/>
 		<Nav />
 		<div>
+			<Slogan />
 			<Welcome />
 			<Translation />
 			<Proofreading />
